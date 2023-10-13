2293835b6d5c807e33ef0c576287fcbab5594e9a
youssef Ahmed <67810732+Youssef8997@users.noreply.github.com>
2023-10-13 19:56:35 +0300

    end

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -24,7 +24,10 @@ class DefaultFirebaseOptions {
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
-        return android;
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for android - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
       case TargetPlatform.iOS:
         return ios;
       case TargetPlatform.macOS:
@@ -49,15 +52,6 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyBoVF1Bc7RlkM-XwFHgx0WqBhkxmAth_qA',
-    appId: '1:164538397597:android:7916274ade863f070db574',
-    messagingSenderId: '164538397597',
-    projectId: 'afeer-2ea3a',
-    databaseURL: 'https://afeer-2ea3a-default-rtdb.firebaseio.com',
-    storageBucket: 'afeer-2ea3a.appspot.com',
-  );
-
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyD3ddg49hu_Kitq9XfNPx6zbravWhxhPiQ',
     appId: '1:164538397597:ios:90579f1a765ad82e0db574',
@@ -65,7 +59,7 @@ class DefaultFirebaseOptions {
     projectId: 'afeer-2ea3a',
     databaseURL: 'https://afeer-2ea3a-default-rtdb.firebaseio.com',
     storageBucket: 'afeer-2ea3a.appspot.com',
-    androidClientId: '164538397597-83qtruieno6hc8968fj6or1rv86l2ofd.apps.googleusercontent.com',
+    androidClientId: '164538397597-2bk15hh29aaivn4v04mu45lge3guj5jf.apps.googleusercontent.com',
     iosClientId: '164538397597-hncruhhm0d1gidbis7hgq9gtgj6i2gco.apps.googleusercontent.com',
     iosBundleId: 'afer.yoyo.com.afeer',
   );
