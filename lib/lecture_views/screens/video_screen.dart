d74e9aac611bf333981ffac612d4da75950fe165
youssef Ahmed <67810732+Youssef8997@users.noreply.github.com>
2023-11-19 13:36:35 +0200

    end

--- lib/lecture_views/screens/video_screen.dart
+++ lib/lecture_views/screens/video_screen.dart
@@ -1,6 +1,7 @@
 import 'package:afeer/utls/extension.dart';
 import 'package:flutter/material.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:pod_player/pod_player.dart';
+//import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class VideoScreen extends StatefulWidget {
 final String videoId;
@@ -11,91 +12,41 @@ final String videoId;
 }
 
 class _VideoScreenState extends State<VideoScreen> {
-  YoutubePlayerController? _controller;
+  late final PodPlayerController controller;
 
 @override
   void initState() {
- _controller = YoutubePlayerController(
-   initialVideoId: widget.videoId,
-   flags: const YoutubePlayerFlags(
-     hideControls: false,
-     showLiveFullscreenButton: true,
-      autoPlay: true,
-      mute: false,
-forceHD: false ,
+  controller = PodPlayerController(
 
+      playVideoFrom: PlayVideoFrom.youtube('https://youtu.be/${widget.videoId}'),
+      podPlayerConfig: const PodPlayerConfig(
+          autoPlay: true,
+          isLooping: false,
+          videoQualityPriority: [720, 360],
 
-    ),
-
-  );
- _controller!.value.playbackQuality="144 p";
-
- _controller?.toggleFullScreenMode();
+      )
+  )..initialise();
  super.initState();
   }
   @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
-    _controller!.value.playbackQuality="144P";
-    _controller!.updateValue(_controller!.value);
 
-    print( _controller!.value.playbackQuality);
 
-  return YoutubePlayerBuilder(
+  return Scaffold(
+    appBar: AppBar(
+      toolbarHeight: 30,
+    ),
+    body: PodVideoPlayer(controller: controller,matchVideoAspectRatioToFrame: true,frameAspectRatio: (context.width/context.height),matchFrameAspectRatioToVideo: true),
+  );
+  }
 
-        player: YoutubePlayer(
-          aspectRatio: (context.height/context.width),
-          controller: _controller!,
-          showVideoProgressIndicator: true,
-
-          progressIndicatorColor: Colors.blueAccent,
-          topActions: <Widget>[
-            const SizedBox(width: 8.0),
-            Text(
-
-              _controller?.value.playbackQuality ?? '',
-            ),
-            Expanded(
-              child: Text(
-                _controller!.metadata.title,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
-                ),
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
-              ),
-            ),
-            const SizedBox(width: 30.0),
-            InkWell(onTap: (){
-              _controller!.toggleFullScreenMode();
-
-              Navigator.pop(context);
-              Navigator.pop(context);
-            },child: const Icon(Icons.arrow_forward_outlined,color: Colors.white,size: 30),)
-          ],
-
-        ),
-        builder: (context, player) => Scaffold(
-            appBar: AppBar(
-              leading: Padding(
-                padding: const EdgeInsets.only(left: 12.0),
-                child: BackButton(
-                  onPressed: (){
-                    Navigator.pop(context);
-                    Navigator.pop(context);
-                  },
-                )
-              ),
-              title: const Text(
-                'Youtube Player Flutter',
-                style: TextStyle(color: Colors.white),
-              ),
-
-            ),
-          body: player,
-        ),);
 
 
   }
-}
+
 
